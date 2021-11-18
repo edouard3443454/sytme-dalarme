dde8eb8cf480da36eb282ae63d53df56c6594e22
edouard3443454 <93147216+edouard3443454@users.noreply.github.com>
2021-11-18 11:50:13 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,22 @@
-let nombre = 0
+input.onButtonPressed(Button.A, function () {
+    état_actuel = 0
+    basic.clearScreen()
+})
 let état_actuel = 0
+état_actuel = 0
 let état_passé = 0
 basic.forever(function () {
-    état_passé = pins.digitalReadPin(DigitalPin.P16)
+    état_passé = pins.digitalReadPin(DigitalPin.P2)
     if (état_actuel != état_passé) {
         if (état_actuel == 1) {
-            nombre += 1
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                . . # . .
+                . . . . .
+                . . # . .
+                `)
         }
     }
     état_passé = état_actuel
-    basic.showNumber(nombre)
 })
